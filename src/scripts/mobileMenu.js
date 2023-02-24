66c7b64709474b2c99c08ba0987e73b92e9a61dd
Hramchenko Alexey <70804795+qwefix@users.noreply.github.com>
2023-02-24 23:19:43 +0300

    done

--- src/scripts/mobileMenu.js
+++ src/scripts/mobileMenu.js
@@ -2,7 +2,6 @@ const mobileMenuController = document.getElementById("mobile-menu-controller");
 const mobileMenu = document.getElementById("mobile-menu");
 const pageHeader = document.getElementById("page-header");
 const body = document.querySelector('body')
-console.log(body)
 let opened = false;
 
 mobileMenuController.addEventListener("click", () => {
@@ -14,3 +13,10 @@ mobileMenuController.addEventListener("click", () => {
   mobileMenu.classList.toggle("opened");
   pageHeader.classList.toggle("opened");
 });
+
+const observer = new IntersectionObserver( 
+  ([e]) => e.target.toggleAttribute('stuck', e.intersectionRatio < 1),
+  {threshold: [1]}
+);
+
+observer.observe(document.getElementById('page-header'));
